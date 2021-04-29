d62c70ad8982938a811c8965068d85bb5d77837b
fuji <fujiyoshiyuki@gmail.com>
2021-04-29 16:55:03 +0900

    修正
    分岐条件追加

--- main.js
+++ main.js
@@ -6,15 +6,29 @@ function execute() {
     var inputRows = inputText.split("\n");
     var leftString = getDocumentId("leftString").value;
     var rightString = getDocumentId("rightString").value;
-    var commentSymbol = getDocumentId("commentSymbol").value;
+    var commentSymbol = getDocumentId("commentSymbol").value == "" ? "//" : getDocumentId("commentSymbol").value;
+    var deleteString;
     for (var i = 0; i < inputRows.length; i++) {
-        // 対象の行でない場合は除く
+        // コメントが指定されている行は除く
         if (!isTargetRow(inputRows[i], commentSymbol)) {
             continue;
         }
-        var deleteString = inputRows[i].replace(leftString, "");
-        var position = deleteString.lastIndexOf(rightString);
-        deleteString = deleteString.substr(0, position);
+        // 左側の文字列が指定されている場合
+        if (leftString.length != 0) {
+            if (isTargetRowLeft(inputRows[i], leftString)) {
+                deleteString = inputRows[i].replace(leftString, "");
+            }
+            else {
+                deleteString = inputRows[i];
+            }
+        }
+        // 右側の文字列が指定されている場合
+        if (rightString.length != 0) {
+            var position = isTargetRowRight(deleteString, rightString);
+            if (position > 0) {
+                deleteString = deleteString.substr(0, position);
+            }
+        }
         inputRows[i] = deleteString;
     }
     var outputTextarea = getDocumentId("outputTextarea");
@@ -28,16 +42,37 @@ function getDocumentId(id) {
     return document.getElementById(id);
 }
 /**
- * 操作すべき行か判定する.
+ * コメント行か判定する.
  * @param text 判定する対象テキスト
- * @param comment コメント文字列
+ * @param commentSymbol コメント判定に使う文字列
  */
-function isTargetRow(text, comment) {
-    if (!text.match(comment)) {
-        return false;
-    }
-    if (text.trim().startsWith(comment)) {
+function isTargetRow(text, commentSymbol) {
+    if (text.trim().startsWith(commentSymbol)) {
         return false;
     }
     return true;
+}
+/**
+ * 左側の文字列が削除対象か判定する.
+ * @param text 判定する対象テキスト
+ * @param leftString 判定に使う文字列
+ */
+function isTargetRowLeft(text, leftString) {
+    if (text.trim().startsWith(leftString)) {
+        return true;
+    }
+    return false;
+}
+/**
+ * 右側の文字列が削除対象か判定する.
+ * @param text 判定する対象テキスト
+ * @param rightString 判定に使う文字列
+ */
+function isTargetRowRight(text, rightString) {
+    var position = 0;
+    if (text.match(rightString)) {
+        var position_1 = text.lastIndexOf(rightString);
+        return position_1;
+    }
+    return position;
 }
--- main.ts
+++ main.ts
@@ -1,26 +1,43 @@
 /**
  * 削除処理を実行する.
  */
-function execute():void{
+ function execute():void{
     const inputText:string = getDocumentId("inputTextarea").value;
     const inputRows:string[] = inputText.split("\n");
     const leftString:string = getDocumentId("leftString").value;
     const rightString:string = getDocumentId("rightString").value;
-    const commentSymbol:string = getDocumentId("commentSymbol").value;
+const commentSymbol:string = getDocumentId("commentSymbol").value == "" ? "//" : getDocumentId("commentSymbol").value;
+var deleteString:string
 
     for (let i = 0; i < inputRows.length; i++){
 
-        // 対象の行でない場合は除く
+        // コメントが指定されている行は除く
         if (!isTargetRow(inputRows[i], commentSymbol)){
             continue;
         }
 
-        var deleteString:string = inputRows[i].replace(leftString,"")
-        const position:number = deleteString.lastIndexOf(rightString)
-        deleteString = deleteString.substr(0,position)
-        inputRows[i] = deleteString;
+        // 左側の文字列が指定されている場合
+        if(leftString.length != 0){
+            if(isTargetRowLeft(inputRows[i], leftString)){
+                deleteString = inputRows[i].replace(leftString,"")
+            }
+            else{
+        deleteString = inputRows[i]
+            }
+        }
 
+
+        // 右側の文字列が指定されている場合
+        if(rightString.length != 0){
+            const position:number = isTargetRowRight(deleteString, rightString)
+            if(position > 0){
+                deleteString = deleteString.substr(0,position)
+            }  
+        }
+        inputRows[i] = deleteString;
+    
     }
+
     let outputTextarea:HTMLInputElement = getDocumentId("outputTextarea");
     outputTextarea.value = inputRows.join("\n");
 }
@@ -34,18 +51,44 @@ function getDocumentId(id:string):HTMLInputElement{
 }
 
 /**
- * 操作すべき行か判定する.
+ * コメント行か判定する.
  * @param text 判定する対象テキスト
- * @param comment コメント文字列
+ * @param commentSymbol コメント判定に使う文字列
  */
- function isTargetRow(text:string, comment:string):boolean{
-    if (!text.match(comment)){
-        return false;
-    }
-    if (text.trim().startsWith(comment)){
+ function isTargetRow(text:string, commentSymbol:string):boolean{
+    if (text.trim().startsWith(commentSymbol)){
         return false;
     }
     return true;
 }
 
+/**
+ * 左側の文字列が削除対象か判定する.
+ * @param text 判定する対象テキスト
+ * @param leftString 判定に使う文字列
+ */
+ function isTargetRowLeft(text:string, leftString:string):boolean{
+    if (text.trim().startsWith(leftString)){
+        return true;
+    }
+    return false;
+}
 
+/**
+ * 右側の文字列が削除対象か判定する.
+ * @param text 判定する対象テキスト
+ * @param rightString 判定に使う文字列
+ */
+ function isTargetRowRight(text:string, rightString:string):number{
+    const position:number = 0;
+
+    if(text.match(rightString)){
+        const position:number = text.lastIndexOf(rightString)
+        return position;
+    }    
+    return position;
+}
+
+
+
+ 
